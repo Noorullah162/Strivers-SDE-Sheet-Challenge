b5c863f77e4e8361ece4dd3340ffb4c8be2d3469
Noorullah162 <113673268+Noorullah162@users.noreply.github.com>
2023-06-06 16:56:43 +0530

    Create check palindrome.cpp

---
+++ Day 1/check palindrome.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		    // Code here.
+		    long long temp = n, res = 0;
+		    while(temp){
+		        res = res*10 + temp % 10;
+		        temp /= 10;
+		    }
+		    return res == n ? "Yes" : "No";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
