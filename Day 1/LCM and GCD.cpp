3168ff102f8e2c67ebc233e22155b35a2108815f
Noorullah162 <113673268+Noorullah162@users.noreply.github.com>
2023-06-06 16:58:21 +0530

    Create LCM and GCD.cpp

---
+++ Day 1/LCM and GCD.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<long long> lcmAndGcd(long long A , long long B) {
+        // code here
+        vector<long long int> v;
+        long long a = A, b = B;
+        while(a!=b){
+            if(a>b) a=a-b;
+            else b=b-a;
+        }
+        v.push_back(A / a * B);
+        v.push_back(a);
+    return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long A,B;
+        
+        cin>>A>>B;
+
+        Solution ob;
+        vector<long long> ans = ob.lcmAndGcd(A,B);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
