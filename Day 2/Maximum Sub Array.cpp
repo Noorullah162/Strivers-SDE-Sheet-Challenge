8e4ebf509bc8fe87f409254e2905264928fc1502
Noorullah162 <113673268+Noorullah162@users.noreply.github.com>
2023-06-06 18:07:01 +0530

    Create Maximum Sub Array.cpp

---
+++ Day 2/Maximum Sub Array.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    long long sum = 0, maxi = LONG_MIN;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+        if(sum > maxi){
+            maxi = sum;
+        }
+        if(sum < 0){
+            sum = 0;
+        }
+    }
+    return maxi<0 ? 0 : maxi;
+}
