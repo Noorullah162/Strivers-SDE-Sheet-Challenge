69e9cc4e2ba7d7a406b89943be195cdabd52840c
Noorullah162 <113673268+Noorullah162@users.noreply.github.com>
2023-06-07 18:34:56 +0530

    Create fact.cpp

---
+++ Day 2/Recursion/fact.cpp
@@ -0,0 +1,48 @@
+// https://practice.geeksforgeeks.org/problems/find-all-factorial-numbers-less-than-or-equal-to-n3548/0?problemType=functional&difficulty%5B%5D=-1&page=1&query=problemTypefunctionaldifficulty%5B%5D-1page1
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    
+    vector<long long> factorialNumbers(long long n)
+    {
+        // Write Your Code here
+        vector<long long int> v;
+        v.push_back(1);
+        if(n==1) return v;
+        for(int i=2; i<=n; i++){
+            if(i*v[i-2]>n)
+                break;
+            v.push_back(i*v[i-2]);
+            // cout << v[i] << " ";
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long N;
+        cin>>N;
+        Solution ob;
+        vector<long long> ans = ob.factorialNumbers(N);
+        for(auto num : ans){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
+// } Driver Code Ends
